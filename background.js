8f18fa7c37f7512b9d8e008a1aeb8c8fe2999d41
ts <tristansetha@gmail.com>
2022-11-19 12:54:40 -0800

    rm console.log background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tab) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.insertCSS(null, { file: "./content.css" });
-    console.log("tabs", tabs)
+    // console.log("tabs", tabs)
     tabs.forEach((tab) => {
       let tabUrl = tab.url;
       chrome.tabs.sendMessage(tab.id, { tabUrl });
